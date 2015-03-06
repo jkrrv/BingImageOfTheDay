d826f3bb46fc9bd1beec8ffd676a33b3ef612f7c
James Kurtz <jkrrv@jkrrv.org>
2015-03-05 23:48:24 -0500

    done-ish

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
-git
+require_once "libs/aCurl/aCurl.php";
+
+$a = new aCurl("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US");
+
+$a->createCurl();
+
+$j = json_decode($a);
+
+try {
+	$img = new aCurl("https://bing.com/".$j->images[0]->url);
+	$img->includeHeader(false);
+	$img->requireContent(false);
+	$img->createCurl();
+	if ($img->getHttpStatus() !=200) {
+		throw new Exception;
+	}
+	header("Content-Type: image/jpeg"); // assumes images are always jpgs  TODO: get from curl header?
+	echo $img;
+} catch (Exception $e) {
+	header("Content-Type: image/jpeg");
+	readfile("down.jpg");
+}
